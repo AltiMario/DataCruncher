b1cbfb782e8f8ba5394fa8bc3ae38eb6a6c603d8
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2016-01-15 15:37:03 +0100

    Small Fix

--- src/main/java/com/seer/datacruncher/spring/MacrosIsActiveController.java
+++ src/main/java/com/seer/datacruncher/spring/MacrosIsActiveController.java
@@ -11,19 +11,20 @@
 
 package com.seer.datacruncher.spring;
 
-import com.seer.datacruncher.jpa.dao.DaoSet;
-import com.seer.datacruncher.jpa.dao.DaoSet;;
-import com.seer.datacruncher.utils.generic.I18n;
-import org.json.JSONObject;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.seer.datacruncher.jpa.dao.DaoSet;
+import com.seer.datacruncher.utils.generic.I18n;
 
 public class MacrosIsActiveController implements Controller, DaoSet {
 
